3ba356a3289a9d1f6352262dfda257c6f05dcfac
vahrameloyan <vahrameloyan@gmail.com>
2017-12-22 16:33:19 +0400

    Added ToDo class

---
+++ ToDoConsole/ToDoConsole/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoConsole
+{
+   public class ToDo
+    {
+        public string Name { set; get; }
+        public string Description { set; get; }       
+        public bool IsDone { set; get; }
+    }
+}
